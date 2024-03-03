ac82af5da8558330db0a31aa3153d311b31ed901
Thanathat Pinthu <thanathatp2003@gmail.com>
2024-03-03 17:19:37 +0700

    Add Service

---
+++ Data/Services/NotificationService.cs
@@ -0,0 +1,42 @@
+﻿using Test.Models;
+
+namespace Test.Data.Services
+{
+    public class NotificationService: INotificationService
+    {
+        private readonly TestDbContext _context;
+        public NotificationService(TestDbContext context)
+        {
+            _context = context;
+        }
+        public void Add(Notification notification)
+        {
+            _context.Notifications.Add(notification);
+            _context.SaveChanges();
+        }
+
+        public void Delete(Notification notification)
+        {
+            _context.Notifications.Remove(notification);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<Notification> GetAll()
+        {
+            var result = _context.Notifications.ToList();
+            return result;
+        }
+
+        public Notification GetById(int id)
+        {
+            var result = _context.Notifications.SingleOrDefault(n => n.Id == id);
+            return result;
+        }
+
+        public void Update(int id, Notification notification)
+        {
+            _context.Notifications.Update(notification);
+            _context.SaveChanges();
+        }
+    }
+}
