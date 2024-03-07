820313f110d1a3e97b241363ccc84ecd7506b9e3
Tanawat Laong <110480443+kodpleum@users.noreply.github.com>
2024-03-08 01:23:59 +0700

    add notification badge red dot & notification data already complete

--- wwwroot/js/backgroundTasks.js
+++ wwwroot/js/backgroundTasks.js
@@ -17,18 +17,33 @@ function runBackgroundTasks(print) {
     xhr2.setRequestHeader('Content-Type', 'application/json');
 
     xhr2.onreadystatechange = function () {
-        if (xhr2.readyState === 4) {
-            if (xhr2.status >= 200 && xhr2.status < 300) {
+        if (xhr2.readyState === 4)
+        {
+            if (xhr2.status >= 200 && xhr2.status < 300)
+            {
                 var notiBtn = document.getElementById("notiBtn");
-                var redDotAppended = document.querySelector(".btnBadge") !== null;
+                var redDotAppended = document.querySelector(".btnBadge") != null;
                 var data = JSON.parse(xhr2.responseText);
+                console.log(data.unread)
                 if (data.unread && !redDotAppended)
                 {
                     var redDot = document.createElement('span');
                     redDot.className = 'btnBadge';
-                    notiBtn.body.appendChild(redDot);
+                    redDot.setAttribute('b-zz1b99kxi4', '');
+                    notiBtn.appendChild(redDot);
                 }
-            } else {
+                else if (!data.unread && redDotAppended)
+                {
+                    // Correctly remove the red dot if there are no unread notifications
+                    var redDot = document.querySelector(".btnBadge[b-zz1b99kxi4]");
+                    if (redDot)
+                    {
+                        redDot.remove();
+                    }
+                }
+            }
+            else
+            {
                 console.error(xhr2.responseText);
             }
         }
