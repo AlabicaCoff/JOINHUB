f2641066cf23ab84277b04d446c7cd3e61303007
Thanathat Pinthu <thanathatp2003@gmail.com>
2024-03-07 16:02:37 +0700

    Fix warnings

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Test.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult NotFoundPage()
+        {
+            return View();
+        }
+    }
+}
